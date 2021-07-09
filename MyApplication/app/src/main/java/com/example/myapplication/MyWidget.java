ba549c5b61576c14b2a856c174afd1c9a8fbbdb4
Wang-YuQuan <79505109+Wang-YuQuan@users.noreply.github.com>
2021-07-09 22:22:08 +0800

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MyWidget.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class MyWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews updateViews = new RemoteViews( context.getPackageName(), R.layout.activity_main);
+            updateViews.setTextViewText(R.id.now,  new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss" ).format(new Date()));
+            appWidgetManager.updateAppWidget(appWidgetIds, updateViews);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context);
+    }
+}
